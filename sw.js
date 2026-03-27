6d6ec88f3196e52923332098f33fe4988df15190
YoDarkol23 <zenix.k11@gmail.com>
2026-03-27 19:07:35 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'vocabulary-cache-v1';
 const ASSETS_TO_CACHE = [
     '/',
-    '/index.html',
-    '/manifest.json',
-    '/icon-192.png',
-    '/icon-512.png'
+    './index.html',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
